44356b779a2c40b72747e1241ce51837199f9200
melyLaur <melissanoemi.laurent@free.fr>
2025-05-11 12:25:03 +0200

    add logger error

--- src/main/java/fr/esgi/doctodocapi/presentation/ErrorHandler.java
+++ src/main/java/fr/esgi/doctodocapi/presentation/ErrorHandler.java
@@ -2,6 +2,8 @@ package fr.esgi.doctodocapi.presentation;
 
 import fr.esgi.doctodocapi.dtos.responses.ErrorResponse;
 import fr.esgi.doctodocapi.exceptions.ApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ExceptionHandler({Exception.class})
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
         ApiException exception = ApiException.from(ex);
         HttpStatus status = exception.getStatus();
         ErrorResponse errorResponse = new ErrorResponse(status.name(), exception.getCode(), exception.getMessage());
+        logger.error("Error : code {}, message {}", errorResponse.code(), errorResponse.message());
         return ResponseEntity.status(status).body(errorResponse);
     }
 }
