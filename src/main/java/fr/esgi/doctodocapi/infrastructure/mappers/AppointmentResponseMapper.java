8633dfcecff732da3c562872e1126c5c91653157
Abdallah Slimane <slimane.abdallah75@gmail.com>
2025-06-25 17:55:35 +0200

    get appointment details

--- src/main/java/fr/esgi/doctodocapi/infrastructure/mappers/AppointmentResponseMapper.java
+++ src/main/java/fr/esgi/doctodocapi/infrastructure/mappers/AppointmentResponseMapper.java
@@ -1,22 +1,20 @@
 package fr.esgi.doctodocapi.infrastructure.mappers;
 
-import fr.esgi.doctodocapi.use_cases.doctor.dtos.responses.appointment_response.GetDoctorAppointmentResponse;
-import fr.esgi.doctodocapi.use_cases.doctor.dtos.responses.appointment_response.MedicalConcernInfo;
-import fr.esgi.doctodocapi.use_cases.doctor.dtos.responses.appointment_response.PatientInfo;
+import fr.esgi.doctodocapi.use_cases.doctor.dtos.responses.appointment_response.*;
 import fr.esgi.doctodocapi.model.appointment.Appointment;
+import fr.esgi.doctodocapi.model.care_tracking.CareTracking;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 public class AppointmentResponseMapper {
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
 
     public GetDoctorAppointmentResponse toResponse(Appointment appointment) {
-        String fullName = String.format("%s %s",
-                appointment.getPatient().getFirstName(),
-                appointment.getPatient().getLastName());
+        String fullName = getPatientFullName(appointment);
 
         return new GetDoctorAppointmentResponse(
                 appointment.getId(),
@@ -38,6 +36,53 @@ public class AppointmentResponseMapper {
                 appointment.getDoctorNotes(),
                 formatDate(appointment.getTakenAt())
         );
+    }
+
+    public GetDoctorAppointmentDetailsResponse toDetailsResponse(Appointment appointment, CareTracking careTracking) {
+        String fullName = getPatientFullName(appointment);
+
+        GetCareTrackingResponse careTrackingResponse = careTracking != null
+                ? GetCareTrackingResponse.fromDomain(careTracking)
+                : null;
+
+        return new GetDoctorAppointmentDetailsResponse(
+                appointment.getId(),
+                new PatientInfo(
+                        appointment.getPatient().getId(),
+                        fullName,
+                        appointment.getPatient().getEmail().getValue(),
+                        appointment.getPatient().getPhoneNumber().getValue(),
+                        appointment.getPatient().getBirthdate().getValue().toString()
+                ),
+                new MedicalConcernInfo(
+                        appointment.getMedicalConcern().getId(),
+                        appointment.getMedicalConcern().getName()
+                ),
+                careTrackingResponse,
+                mapAnswers(appointment),
+                appointment.getDate().toString(),
+                appointment.getHoursRange().getStart().toString(),
+                appointment.getStatus().getValue(),
+                appointment.getDoctorNotes(),
+                formatDate(appointment.getTakenAt())
+        );
+    }
+
+    private String getPatientFullName(Appointment appointment) {
+        return String.format("%s %s",
+                appointment.getPatient().getFirstName(),
+                appointment.getPatient().getLastName());
+    }
+
+    private List<AnswerInfo> mapAnswers(Appointment appointment) {
+        return appointment.getPreAppointmentAnswers() != null
+                ? appointment.getPreAppointmentAnswers().stream()
+                .map(answer ->
+                        new AnswerInfo(
+                                answer.getQuestion().getId(),
+                                answer.getResponse()
+                        )
+                ).toList() : null;
     }
 
     private String formatDate(LocalDateTime date) {
