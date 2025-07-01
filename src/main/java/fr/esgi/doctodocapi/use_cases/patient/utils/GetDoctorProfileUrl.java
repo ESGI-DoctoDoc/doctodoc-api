3696cf53776a0804ac4b4b12bb3327049db66a73
melyLaur <melissanoemi.laurent@free.fr>
2025-07-01 21:13:13 +0200

    get doctor profile url

--- src/main/java/fr/esgi/doctodocapi/use_cases/patient/utils/GetDoctorProfileUrl.java
+++ src/main/java/fr/esgi/doctodocapi/use_cases/patient/utils/GetDoctorProfileUrl.java
@@ -2,10 +2,14 @@ package fr.esgi.doctodocapi.use_cases.patient.utils;
 
 import fr.esgi.doctodocapi.use_cases.patient.ports.out.FileNotExistedException;
 import fr.esgi.doctodocapi.use_cases.patient.ports.out.FileStorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetDoctorProfileUrl {
+    private static final Logger logger = LoggerFactory.getLogger(GetDoctorProfileUrl.class);
+
     private final FileStorageService fileStorageService;
 
     public GetDoctorProfileUrl(FileStorageService fileStorageService) {
@@ -18,7 +22,7 @@ public class GetDoctorProfileUrl {
         try {
             profileUrl = this.fileStorageService.getFile(path);
         } catch (FileNotExistedException e) {
-//            logger.info("Error : code {}, message {}", e.getCode(), e.getMessage());
+            logger.info("Cannot get doctor profile url : code {}, message {}", e.getCode(), e.getMessage());
         }
         return profileUrl;
     }
