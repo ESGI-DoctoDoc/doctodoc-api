12aeaf21a38c6cff4b44d1c70289b3b141a08247
melyLaur <melissanoemi.laurent@free.fr>
2025-07-07 00:23:56 +0200

    rollback findFirstPaidSubscriptionByDoctor

--- src/main/java/fr/esgi/doctodocapi/infrastructure/jpa/repositories/DoctorSubscriptionJpaRepository.java
+++ src/main/java/fr/esgi/doctodocapi/infrastructure/jpa/repositories/DoctorSubscriptionJpaRepository.java
@@ -5,23 +5,23 @@ import fr.esgi.doctodocapi.infrastructure.jpa.entities.DoctorSubscriptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DoctorSubscriptionJpaRepository extends JpaRepository<DoctorSubscriptionEntity, UUID> {
-    @Query(value = """
-            SELECT s.*
-            FROM doctor_subscriptions s
-            JOIN doctor_invoices i ON i.subscription_id = s.id
-            WHERE s.doctor_id = :doctorId
-            AND s.end_date::date >= CAST(:now AS date)
-            AND i.state = 'PAID'
-            ORDER BY s.end_date DESC
-            LIMIT 1
-            """, nativeQuery = true)
-    Optional<DoctorSubscriptionEntity> findFirstPaidSubscriptionByDoctor(UUID doctorId, LocalDate now);
+    @Query("""
+                SELECT s
+                FROM DoctorSubscriptionEntity s
+                JOIN DoctorInvoiceEntity i ON i.subscription = s
+                WHERE s.doctor = :doctor
+                AND s.endDate > :now
+                AND i.state = 'PAID'
+                ORDER BY s.endDate DESC
+                LIMIT 1
+            """)
+    Optional<DoctorSubscriptionEntity> findFirstPaidSubscriptionByDoctor(DoctorEntity doctor, LocalDateTime now);
 
 
     @Query("""
